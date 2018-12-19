0a0825a043bb30de8cf3de72c5ea9622218523b6
Joseph Broton <jbroton2@illinois.edu>
2018-12-19 01:10:25 +0000

    added the correct app.R file

--- twitter_analysis_code/app.R
+++ twitter_analysis_code/app.R
@@ -1,18 +1,13 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# STAT 385 Group Project
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-#install.packages("shiny")
-library(shiny)
 
+
+library(shiny)
 
 #####---- define Function section ----####
 
-pkg_list = c("rtweet", "ggplot2", "tidyr", "dplyr", "tidytext", "tm", "widyr", "RColorBrewer", "igraph", "twitteR", "httr", "wordcloud", "base64enc", "openssl", "httpuv")
+pkg_list = c("rtweet", "ggplot2", "tidyr", "dplyr", "tidytext", "tm", "widyr", "RColorBrewer", "reshape2", "syuzhet", "shinyjs", "igraph", "twitteR", "httr", "wordcloud", "base64enc", "openssl", "httpuv", "DT")
 mia_pkgs = pkg_list[!(pkg_list %in% installed.packages()[,"Package"])]
 if(length(mia_pkgs) > 0) install.packages(mia_pkgs)
 loaded_pkgs = lapply(pkg_list, require, character.only=TRUE)
@@ -28,6 +23,9 @@ searching <- function(search_string) {
     setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
     tw <- searchTwitter(search_input, n = 1e6, since = '2016-06-08', retryOnRateLimit = 1e3)
     d <- twListToDF(tw)
+}
+
+text2word <- function(d) {
     text_df <- data_frame(line=1:length(d$text), text=c(d$text))
     words <- text_df %>%
         unnest_tokens(word, text)
@@ -41,11 +39,11 @@ merge.words.dictionary <- function(words) {
         summarise(sentiment = sum(score)) %>%
         mutate(method = "AFINN")
     bing_and_nrc <- bind_rows(words %>%
-                                  inner_join(get_sentiments("bing")) %>%
-                                  mutate(method = "Bing"), words %>%
-                                  inner_join(get_sentiments("nrc") %>%
-                                                 filter(sentiment %in% c("positive", "negative"))) %>%
-                                  mutate(method = "NRC")) %>%
+        inner_join(get_sentiments("bing")) %>%
+        mutate(method = "Bing"), words %>%
+        inner_join(get_sentiments("nrc") %>%
+        filter(sentiment %in% c("positive", "negative"))) %>%
+        mutate(method = "NRC")) %>%
         count(method, index = line, sentiment) %>%
         spread(sentiment, n, fill = 0) %>%
         mutate(sentiment = positive - negative)
@@ -59,7 +57,7 @@ making.plots <- function(words, dict) {
     if (length(dict) > 1) {
         for (i in c(2:length(dict))) {
             subset <- rbind(subset, all[all$method == dict[i], ])
-        }}
+    }}
     subset %>%
         ggplot(aes(index, sentiment, fill = method)) +
         geom_col(show.legend = FALSE) +
@@ -68,45 +66,116 @@ making.plots <- function(words, dict) {
 
 
 # Define UI for application that draws a histogram
-myui <- fluidPage(
-    titlePanel("STAT 385 Group Project"),
-    sidebarLayout(
-        sidebarPanel(
-            textInput(inputId = "Search",    label = "Search Strings"),
+myui <- fluidPage(theme = shinytheme("flatly"), 
+   tags$title("STAT 385 Group Project - Sentiment of Twitter"),
+   
+   column(width = 12,
+          h1("Sentiment Analysis of Twitter API")
+   ),
+
+    sidebarPanel(
+            radioButtons("radio", label = h3("Tweet Data"),
+                   choices = list("Default Data" = 1, "New Search" = 2), 
+                   selected = 1),
+            
+            h3("If new search - Input search String:"), 
+            textInput(inputId = "Search",    label = "Search String"),
+            
+            hr(),
             actionButton("do","Run Sentiment Analysis"),
             checkboxGroupInput("Dictionary", h3("Dictionary choices"),
-                               choices = list("AFINN" = "AFINN", "Bing" = "Bing", "NRC" = "NRC"), selected ="AFINN")
+                choices = list("AFINN" = "AFINN", "Bing" = "Bing", "NRC" = "NRC"), selected ="AFINN"),
+            helpText("To change the default score, click on the cells in the last column below."),
+            h3("Score Data Table"),
+            DT::dataTableOutput('x1')
+            
         ),
-        mainPanel(
-            tabsetPanel(
-                type="tabs",
-                tabPanel("Plot",         plotOutput(outputId = "plot")),
-                tabPanel("cloud",        plotOutput(outputId = "cloud")),
-                tabPanel("Bing Words",         plotOutput(outputId = "bingword")),
-                tabPanel("Score Distribution",         plotOutput(outputId = "scores"))
-            )
-        )
-    )
+      mainPanel(
+        fluidRow(
+          
+          #column(width=6, h4("Word Cloud"),
+          #      plotOutput(outputId = "cloud", height=250)),
+          
+          column(width=4, h4("Bing Words"),
+                 plotOutput(outputId = "bingword", height=280)),
+          
+          column(width=4, h4("Score Distribution"),
+                 plotOutput(outputId = "scores", height=280)),
+          
+          column(width=4, h4("User Score Distribution"),
+                 plotOutput(outputId = "newscores", height=280)),
+          
+          column(width=12, h4("Sentiment Lexicons"),
+                 plotOutput(outputId = "plot", height=300)),
+          
+          column(width=6, h3("By Tweet"),
+                 htmlOutput(outputId = "most")),      
+                 tags$head(tags$style("#most{color: purple;
+                                 font-size: 20px;
+                                 }")),
+          
+          column(width=6, h4("By Tweet - Distribution"),
+                 plotOutput(outputId = "tweets", height=250))     
+        )   
+   )
 )
 # Define server logic required to draw a histogram
 myserver <- function(input, output) {
     observeEvent(input$do, {
-        search.out <- reactive({
+        
+        if (input$radio == "1" ) {
+          load("twitter_data.Rda")
+          d1 <- rt
+        }
+        else {
+            search.out <- reactive({
             searching(input$Search)
-        })
+            })
+            d1 <- search.out()
+        }
+        tw <- text2word(d1)
+       
+        tweets.df <- gsub("http.*","",d1$text)
+        tweets.df <- gsub("https.*","",tweets.df)
+        tweets.df <- gsub("#.*","",tweets.df)
+        tweets.df <- gsub("@.*","",tweets.df)
+        word.df <- as.vector(tweets.df)
+        word.df <- gsub("[^[:alnum:]///' ]", "", word.df)
+        emotion.df <- get_nrc_sentiment(word.df,  language = "english")
+        emotion.df2 <- cbind(tweets.df, emotion.df) 
+        
+        sent.value <- get_sentiment(word.df, method="afinn")
+        most.positive <- word.df[sent.value == max(sent.value)][1]
+        
+        most.negative <- word.df[sent.value <= min(sent.value)][1]
+        
+        category_senti <- ifelse(sent.value < 0, "Negative", ifelse(sent.value > 0, "Positive", "Neutral"))
+        senti_summary <- table(category_senti)
+        
+        senti_summary <- as.data.frame(senti_summary)
+       
+        output$tweets <- renderPlot(
+           ggplot(data=senti_summary, aes(x=category_senti, y=Freq, fill=category_senti)) +
+           geom_bar(stat="identity")
+        )
+        
+        output$most <-
+          renderText(
+            HTML(paste("<b>", "The most positive:", most.positive, "<br/>", "<br/>", "The most negative:", most.negative, "</b>")))
+        
         output$plot <- renderPlot(
-            making.plots(search.out(), input$Dictionary)
+            making.plots(tw, input$Dictionary)
         )
         output$cloud <- renderPlot(
-            search.out() %>%
+            tw %>%
                 inner_join(get_sentiments("bing")) %>%
                 count(word, sentiment, sort = TRUE) %>%
                 acast(word ~ sentiment, value.var = "n", fill = 0) %>%
                 comparison.cloud(colors = c("gray20", "gray80"),
-                                 max.words = 5000)
+                             max.words = 100)
         )
         output$bingword <- renderPlot(
-            search.out() %>%
+            tw %>%
                 inner_join(get_sentiments("bing")) %>%
                 count(word, sentiment, sort = TRUE) %>%
                 ungroup() %>%
@@ -121,17 +190,50 @@ myserver <- function(input, output) {
                      x = NULL) +
                 coord_flip()
         )
+        
+        x0 <- tw %>% count(word, sort = TRUE) %>% inner_join(get_sentiments("afinn"))
+        x0 <- as.data.frame(x0)
+        x0$newscore <- x0$score
+        x <- x0
+        
         output$scores <- renderPlot(
-            search.out() %>%
-                count(word, sort = TRUE) %>%
-                inner_join(get_sentiments("afinn"), by = NULL) %>%
-                group_by(score) %>%
-                summarize(Count = sum(n)) %>%
-                ggplot(aes(x=as.factor(score), y=Count)) +
-                geom_bar(stat="identity", show.legend = FALSE, fill="steelblue") +
-                labs(y = "Count", x = "Score")
+          x0 %>%
+            group_by(score) %>%
+            summarize(Count = sum(n)) %>%
+            ggplot(aes(x=as.factor(score), y=Count)) +
+            geom_bar(stat="identity", show.legend = FALSE, fill="steelblue") +
+            labs(y = "Count", x = "Score")
         )
+        
+        output$x1 = DT::renderDataTable(x, selection = 'none', editable = TRUE)
+        
+        proxy = dataTableProxy('x1')
+        y <- reactive({
+          input$x1_cell_edit
+          x
+        })
+        
+        observeEvent(input$x1_cell_edit, {
+          info = input$x1_cell_edit
+          str(info)
+          i = info$row
+          j = info$col
+          v = info$value
+          x[i, j] <<- DT::coerceValue(v, x[i, j])
+          replaceData(proxy, x, resetPaging = FALSE)
+        })
+        
+        output$newscores <- renderPlot(
+          y() %>%
+            group_by(newscore) %>%
+            summarize(Count = sum(n)) %>%
+            ggplot(aes(x=as.factor(newscore), y=Count)) +
+            geom_bar(stat="identity", show.legend = FALSE, fill="orange") +
+            labs(y = "Count", x = "New Score")
+        )
+        
     })
+  
 }
 
 # Run the application
